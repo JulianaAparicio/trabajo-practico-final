0bb30a3691374e9d45c8d640f4fe30e74b221549
JulianaAparicio <juliana_aparicio@outlook.com>
2023-06-24 20:32:16 -0300

    Added new method to ms bills repository

--- Proyecto/ms-bills/src/main/java/com/dh/msbills/repositories/BillRepository.java
+++ Proyecto/ms-bills/src/main/java/com/dh/msbills/repositories/BillRepository.java
@@ -4,7 +4,10 @@ import com.dh.msbills.models.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BillRepository extends JpaRepository<Bill, String> {
+    List<Bill> findAllByCustomerBill(String customerId);
 
 }
