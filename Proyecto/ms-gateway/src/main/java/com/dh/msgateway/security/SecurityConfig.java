d3cdeb81dba8e5845941bbfb81f525cdcb83630a
JulianaAparicio <juliana_aparicio@outlook.com>
2023-06-25 23:00:28 -0300

    Fixed SecurityConfig

--- Proyecto/ms-gateway/src/main/java/com/dh/msgateway/security/SecurityConfig.java
+++ Proyecto/ms-gateway/src/main/java/com/dh/msgateway/security/SecurityConfig.java
@@ -2,21 +2,36 @@ package com.dh.msgateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+        http.cors().and().csrf().disable();
+
         http
-                .cors().and().csrf().disable()
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtDecoder(jwtDecoder())))
                 .authorizeExchange()
                 .anyExchange()
+                // Para que todos los usuarios que accedan esten autenticados:
                 .authenticated()
                 .and()
+                // Para redirigir a la página de oauth:
                 .oauth2Login();
 
         return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder
+                .withJwkSetUri("http://localhost:8080/realms/EcommerceAparicio/protocol/openid-connect/certs")
+                .build();
     }
 }
