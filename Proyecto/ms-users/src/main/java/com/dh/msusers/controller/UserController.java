4e7de519504261dd8fc0d9bc5cb85d0ea0ed6843
JulianaAparicio <juliana_aparicio@outlook.com>
2023-06-26 21:54:06 -0300

    Added Controller class

---
+++ Proyecto/ms-users/src/main/java/com/dh/msusers/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.dh.msusers.controller;
+
+import com.dh.msusers.model.User;
+import com.dh.msusers.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/findById")
+    public ResponseEntity<User> getAll(@RequestParam String customerBill) {
+        return ResponseEntity.ok().body(userService.getAllBill(customerBill));
+    }
+}
